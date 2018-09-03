21245731236005698546415825823ca70c2e77b4
Ulises <ulis2_rom@hotmail.com>
2018-09-03 11:24:03 -0500

    Agregar archivos de proyecto.

---
+++ Nombre de consola/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nombre_de_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
